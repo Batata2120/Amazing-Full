84645c11dac4eb806b5b24a54024834404b63555
Batata2120 <mateus@pop-os.localdomain>
2023-11-15 10:38:55 -0300

    Adicionando a opção de listar

--- frontend/javascript/webserviceConexao.js
+++ frontend/javascript/webserviceConexao.js
@@ -541,25 +541,28 @@ function delProdutosCompra(id) {
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
+///////////////
+//Função listar
+///////////////
 
-function listarClientes(objeto, styleDiv = ""){
+function listarClientes(objeto, styleDiv = "") {
     let clientes = getClientes();
     console.log(clientes);
-    clientes.then(lista =>{
+    clientes.then(lista => {
         console.log(lista);
-        while(objeto.nextElementSibling.children.length > 0){
+        while (objeto.nextElementSibling.children.length > 0) {
             objeto.nextElementSibling.removeChild(objeto.nextElementSibling.children[objeto.nextElementSibling.children.length - 1]);
         }
-        for(elemento of lista){
+        for (elemento of lista) {
             let cliente = document.createElement("div");
             let vetor = Array();
             let chaves = Object.keys(elemento);
-            for(chave of chaves){
+            for (chave of chaves) {
                 let dive = document.createElement("div");
                 dive.innerText = elemento[chave];
                 vetor.push(dive);
             }
-            for(valor of vetor){
+            for (valor of vetor) {
                 cliente.append(valor);
             }
             cliente.style = styleDiv;
@@ -567,8 +570,131 @@ function listarClientes(objeto, styleDiv = ""){
         }
     })
 }
-let botao = document.querySelector(".listarTodosCliente");
-botao.addEventListener("click", function (e){
+
+function listarProdutos(objeto, styleDiv = "") {
+    let produtos = getProdutos();
+    console.log(produtos);
+    produtos.then(lista => {
+        console.log(lista);
+        while (objeto.nextElementSibling.children.length > 0) {
+            objeto.nextElementSibling.removeChild(objeto.nextElementSibling.children[objeto.nextElementSibling.children.length - 1]);
+        }
+        for (elemento of lista) {
+            let produto = document.createElement("div");
+            let vetor = Array();
+            let chaves = Object.keys(elemento);
+            for (chave of chaves) {
+                let dive = document.createElement("div");
+                dive.innerText = elemento[chave];
+                vetor.push(dive);
+            }
+            for (valor of vetor) {
+                produto.append(valor);
+            }
+            produto.style = styleDiv;
+            objeto.nextElementSibling.append(produto);
+        }
+    })
+}
+
+function listarCompras(objeto, styleDiv = "") {
+    let compras = getCompras();
+    console.log(compras);
+    compras.then(lista => {
+        console.log(lista);
+        while (objeto.nextElementSibling.children.length > 0) {
+            objeto.nextElementSibling.removeChild(objeto.nextElementSibling.children[objeto.nextElementSibling.children.length - 1]);
+        }
+        for (elemento of lista) {
+            let compra = document.createElement("div");
+            let vetor = Array();
+            let chaves = Object.keys(elemento);
+            for (chave of chaves) {
+                let dive = document.createElement("div");
+                dive.innerText = elemento[chave];
+                vetor.push(dive);
+            }
+            for (valor of vetor) {
+                compra.append(valor);
+            }
+            compra.style = styleDiv;
+            objeto.nextElementSibling.append(compra);
+        }
+    })
+}
+
+function listarProdutosCompras(objeto, styleDiv = "") {
+    let produtosCompras = getProdutosCompras();
+    console.log(produtosCompras);
+    produtosCompras.then(lista => {
+        console.log(lista);
+        while (objeto.nextElementSibling.children.length > 0) {
+            objeto.nextElementSibling.removeChild(objeto.nextElementSibling.children[objeto.nextElementSibling.children.length - 1]);
+        }
+        for (elemento of lista) {
+            let produtosCompra = document.createElement("div");
+            let vetor = Array();
+            let chaves = Object.keys(elemento);
+            for (chave of chaves) {
+                let dive = document.createElement("div");
+                dive.innerText = elemento[chave];
+                vetor.push(dive);
+            }
+            for (valor of vetor) {
+                produtosCompra.append(valor);
+            }
+            produtosCompra.style = styleDiv;
+            objeto.nextElementSibling.append(produtosCompra);
+        }
+    })
+}
+
+////Botões///
+
+let botaoCliente = document.querySelector(".listarTodosCliente");
+botaoCliente.addEventListener("click", function (e) {
     let elemento = e.target;
-    listarClientes(elemento.parentElement);   
+    listarClientes(elemento.parentElement);
 })
+let botaoProduto = document.querySelector(".listarTodosProdutos");
+botaoProduto.addEventListener("click", function (e) {
+    let elemento = e.target;
+    listarProdutos(elemento.parentElement);
+})
+let botaoCompra = document.querySelector(".listarTodasCompras");
+botaoCompra.addEventListener("click", function (e) {
+    let elemento = e.target;
+    listarCompras(elemento.parentElement);
+})
+let botaoProdutoCompra = document.querySelector(".listarTodosProdutosCompra");
+botaoProdutoCompra.addEventListener("click", function (e) {
+    let elemento = e.target;
+    listarProdutosCompras(elemento.parentElement);
+})
+
+////////////////////
+///Função inserir///
+////////////////////
+
+function inserirCliente(nome, nome_usuario, estado, cidade, bairro, rua, nro_cartao, nro_seguranca, nome_cartao, data_validade_cartao) {
+    return (addCliente(nome, nome_usuario, estado, cidade, bairro, rua, nro_cartao, nro_seguranca, nome_cartao, data_validade_cartao));
+}
+
+
+///////Botões
+let botaoInserirCliente = document.querySelector(".inserirCliente");
+botaoInserirCliente.addEventListener("click", function (e) {
+    let elemento = e.target;
+    let paiElemento = elemento.parentElement;
+    let filhos = paiElemento.children;
+    let campos = ["nome", "nome_usuario", "estado", "cidade", "bairro", "rua", "nro_cartao", "nro_seguranca", "nome_cartao", "data_validade_cartao"];
+    valores = [];
+    for (valor of filhos) {
+        for (campo of campos) {
+            if (valor.name == campo) {
+                valores.push(valor.value);
+            }
+        }
+    }
+    inserirCliente(valores[0], valores[1], valores[2], valores[3], valores[4], valores[5], valores[6], valores[7], valores[8])
+});
